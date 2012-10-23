2f7fd22d2d70539b933541a541cccc3e057e3557
Ben Rexin <salzig+github@gmail.com>
2012-10-23 13:51:56 +0200

    create ~/Library/KeyBindings if needed

--- env/keybindings.sh
+++ env/keybindings.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 FILE="$HOME/Library/KeyBindings/DefaultKeyBinding.dict"
+PATH=`dirname $FILE`
 
+/bin/mkdir -p $PATH
 
 /usr/libexec/PlistBuddy -c 'Add :"~ " string ("insertText:", " ")' $FILE
 /usr/bin/plutil -convert json -r $FILE
