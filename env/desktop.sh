b42315a6950531056da13e1de458d0dcf7ebf94e
Ben Rexin <ben@rexin.at>
2020-09-24 07:34:27 +0200

    feat: no floating screenshot tool, store immediately

--- env/desktop.sh
+++ env/desktop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# no desktop icons
 defaults write com.apple.finder CreateDesktop -bool false
 killall Finder
+
+# no floating screenshot tool, store immediately
+defaults write com.apple.screencapture show-thumbnail -bool false
 
